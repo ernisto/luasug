45a9ae150e9751e1d6a57a76a5d1c1297cca8d08
ernisto <joaovitordesouza707@gmail.com>
2023-11-04 01:52:49 -0300

    remove test

--- src/init.lua
+++ src/init.lua
@@ -4,9 +4,6 @@ local Lexer = require("Lexer/init.lua")
         require("Parser/Stat.lua")
 
 --// Run
-local a = Lexer.new("10"):scanDecNumber()
-print(a)
-
 local lexer = Lexer.new([[
 local a = 10
 ]])
