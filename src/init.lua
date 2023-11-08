d46bed1d059b13af9ab0b91dbcf7de803b12b90e
ernisto <joaovitordesouza707@gmail.com>
2023-11-08 00:18:22 -0300

    update test

--- src/init.lua
+++ src/init.lua
@@ -4,12 +4,8 @@ local Lexer = require("Lexer/init.lua")
         require("Parser/Stat.lua")
 
 --// Run
-local lexer = Lexer.new([[
-local a = 10
-]])
+local lexer = Lexer.new([[local a = 10]])
 local tokenStream = lexer:tokenize()
-print(tokenStream.tokens)
-
 local parser = tokenStream:parse()
 local ast = parser:body()
 
